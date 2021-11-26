e1c8daed0513f39d1b07357dd4510aa735de994f
LiberViator <54078423+LiberViator@users.noreply.github.com>
2021-11-26 21:54:05 +0100

    Rating changed to levels

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-const appHeight = () => {
-    document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`)
-}
-window.addEventListener('resize', appHeight)
-appHeight()
-
-
 const catalogue = document.querySelector(".catalogue");
 
 const url = "./models.json";
@@ -18,8 +11,6 @@ async function fetchJSON() {
 fetchJSON().then(data => {
   for (let card = 0; card < data.length; card++) {
 
-    console.log('Walking east one step');
-
     const newCard = document.createElement("a");
     newCard.classList.add("catalogue__card");
     newCard.style.backgroundImage = "url(\"" + data[card].background + "\")";
@@ -29,15 +20,14 @@ fetchJSON().then(data => {
       window.location.href = "./tutorial.html" + "?id=" + card;
     };
 
-    const newCardRating = document.createElement("div");
-    newCardRating.classList.add("catalogue__card__rating");
-    newCard.appendChild(newCardRating);
+    const newCardLevel = document.createElement("div");
+    newCardLevel.classList.add("catalogue__card__level");
+    newCard.appendChild(newCardLevel);
 
-    const newCardRatingIcon = document.createElement("img");
-    newCardRatingIcon.classList.add("catalogue__card__rating__icon");
-    newCardRatingIcon.src = "img/heart.svg";
-    newCardRating.appendChild(newCardRatingIcon);
-
+    const newCardLevelIcon = document.createElement("img");
+    newCardLevelIcon.classList.add("catalogue__card__level__icon");
+    newCardLevelIcon.src = "img/" + data[card].level + ".svg";
+    newCardLevel.appendChild(newCardLevelIcon);
 
     const newCardTitle = document.createElement("div");
     newCardTitle.classList.add("catalogue__card__title");
--- tutorial.js
+++ tutorial.js
@@ -1,7 +1,9 @@
+const tutorialLevelIcon = document.querySelector(".tutorial__menu__level__icon");
 const tutorialTitle = document.querySelector(".tutorial__menu__info__title");
 const tutorialSteps = document.querySelector(".tutorial__menu__info__steps");
 const backButton = document.querySelector(".tutorial__nav__back");
 const nextButton = document.querySelector(".tutorial__nav__next");
+const nextButtonText = document.querySelector(".tutorial__nav__next__text");
 const animation = document.querySelector(".animation");
 
 const id = location.search.substring(0).replace("?id=", "");
@@ -17,34 +19,39 @@ async function fetchJSON() {
 }
 
 fetchJSON().then(data => {
+  document.title = data[id].name;
+
   for (let card = 0; card <= id; card++) {
-
-    console.log('Walking east one step');
-
+    tutorialLevelIcon.src = "img/" + data[id].level + ".svg";
     tutorialTitle.innerHTML = data[id].name;
     tutorialSteps.innerHTML = step + " / " + data[id].steps.length;
-
-    backButton.onclick = function() {
-      if (step > 1) {
-        step--;
-        tutorialSteps.innerHTML = step + " / " + data[id].steps.length;
-      } else {
-        // Back to catalogue
-        window.location.href = "./index.html";
-      }
-    };
-
-    nextButton.onclick = function() {
-      if (step < data[id].steps.length) {
-        step++;
-        tutorialSteps.innerHTML = step + " / " + data[id].steps.length;
-      } else {
-        // Play animation and back to catalogue
-        animation.classList.add("running")
-        setTimeout(function playAnimation() {
-          window.location.href = "./index.html";
-        }, 800);
-      }
-    };
   }
+
+  backButton.onclick = function() {
+    if (step > 1) {
+      step--;
+      tutorialSteps.innerHTML = step + " / " + data[id].steps.length;
+      nextButtonText.innerHTML = "Next step";
+    } else {
+      // Back to catalogue
+      window.location.href = "./index.html";
+    }
+  };
+
+  nextButton.onclick = function() {
+    if (step < data[id].steps.length) {
+
+      step++;
+      tutorialSteps.innerHTML = step + " / " + data[id].steps.length;
+    } else {
+      // Play animation and back to catalogue
+      animation.classList.add("running")
+      setTimeout(function playAnimation() {
+        window.location.href = "./index.html";
+      }, 800);
+    }
+    if (step === data[id].steps.length) {
+      nextButtonText.innerHTML = "Finish";
+    }
+  };
 });
